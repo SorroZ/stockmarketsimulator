b404e41762eeb71be54981ec98a49b455690d719
Daniel Johansson <danne.joh1@gmail.com>
2015-12-26 22:32:10 +0100

    Added a.o. curses for display

--- smsim.py
+++ smsim.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python
 import matplotlib.pyplot as plt
+import time
+import sys
+import curses
 
 from stock import Stock
 #------------------------------------------
 
-timeArray = [0]
-s = Stock("Test", "T", 100)
-for i in xrange(1,1000):
-    timeArray.append(i)
-    s.changeByTime()
+if __name__ == "__main__":
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+
+    timeArray = [0]
+    s = Stock("Test", "T", 100)
+    for i in xrange(1,1000):
+        timeArray.append(i)
+        s.changeByTime()
+        stdscr.addstr(0, 0, s.getPerformance())
+        time.sleep(0.5);
+        stdscr.refresh()
 
 
-#print s.getAllValues()
+print s.getPerformance()
 
 
-plt.plot(timeArray, s.getAllValues())
-#plt.axis([0, 6, 0, 20])
+#plt.plot(timeArray, s.getAllValues())
+plt.plot([0, 6, 0, 20], [0,1,2,3])
+plt.plot([1, 7, 1, 21], [0,1,2,3])
 plt.show()
